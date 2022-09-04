21c4248dd36c4d107668b857dd55d13b9a4b0f0e
Jakob Janzen <jakob.janzen80@gmail.com>
2022-09-04 21:25:28 +0200

    changed brace position

--- Java_Basic_Programs/src/FibonacciExample1.java
+++ Java_Basic_Programs/src/FibonacciExample1.java
@@ -2,7 +2,7 @@
  * 
  * @author Jakob Janzen <jakob.janzen80@gmail.com>
  *
- * Fibonacci Series in Java without using recursion.
+ *         Fibonacci Series in Java without using recursion.
  */
 
 public class FibonacciExample1 {
@@ -15,8 +15,7 @@ public class FibonacciExample1 {
 		int n1 = 0, n2 = 1, n3, i, count = 20;
 		System.out.print(n1 + " " + n2); // printing 0 and 1
 
-		for (i = 2; i < count; ++i) // loop starts from 2 because 0 and 1 are already printed
-		{
+		for (i = 2; i < count; ++i) { // loop starts from 2 because 0 and 1 are already printed
 			n3 = n1 + n2;
 			System.out.print(" " + n3);
 			n1 = n2;
