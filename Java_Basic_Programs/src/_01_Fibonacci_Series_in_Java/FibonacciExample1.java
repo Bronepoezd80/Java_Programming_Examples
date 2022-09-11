9de0c861c0b8ccbc81fca9e96745932e32375f63
Jakob Janzen <jakob.janzen80@gmail.com>
2022-09-11 21:40:55 +0200

    simplified algorithm

--- Java_Basic_Programs/src/_01_Fibonacci_Series_in_Java/FibonacciExample1.java
+++ Java_Basic_Programs/src/_01_Fibonacci_Series_in_Java/FibonacciExample1.java
@@ -8,22 +8,21 @@ package _01_Fibonacci_Series_in_Java;
  */
 public class FibonacciExample1 {
 
+  public static int count = 20;
+
   /**
    * 
    * @param args
    */
   public static void main(String[] args) {
     General.Description.print(1, "Fibonacci Series in Java without using recursion");
-    int n1 = 0, n2 = 1, n3, i, count = 20;
-    System.out.print(n1 + " " + n2); // printing 0 and 1
 
-    for (i = 2; i < count; ++i) { // loop starts from 2 because 0 and 1 are already printed
-      n3 = n1 + n2;
-      System.out.print(" " + n3);
-      n1 = n2;
-      n2 = n3;
+    for (int i = 0, n1 = 0, n2 = 1; i < count; ++i) {
+      System.out.print(n2 + " ");
+      n2 += n1;
+      n1 = n2 - n1;
     }
-
+    System.out.println();
   }
 
 }
